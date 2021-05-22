d4f10ef4e8bf4dfaaa701420b5c107c8e01326d5
zodiacapricorn <84628562+zodiacapricorn@users.noreply.github.com>
2021-05-22 18:33:00 +0200

    Update tabelle.sql

--- CODE/tabelle.sql
+++ CODE/tabelle.sql
@@ -5,7 +5,7 @@ CREATE TABLE CASA_AUTOMOBILISTICA (
   nazionalita enum('afghana','albanese','algerina','andorrana','angolana','argentina','armena','australiana','austriaca','azera','bahamense','bahreinita','barbadiana','belga','bengalese','beninese','bhutanese','bielorussa','birmana','boliviana','bosniaca','botswana','brasiliana','britannica','bruneiana','bulgara','burkinabÃ©','burundese','cambogiana','camerunense','canadese','capoverdiana','ceca','centrafricana','ciadiana','cilena','cinese','cingalese','cipriota','colombiana','comoriana','congolese','costaricana','croata','cubana','danese','dominicana','ecuadoregna','egiziana','emiratina','eritrea','estone','etiope','figiana','filippina','finlandese','francese','gabonese','gambiana','georgiana','ghanese','giamaicana','giapponese','gibutiana','giordana','greca','grenadina','guatemalteca','guineana','guyanese','haitiana','honduregna','indiana','indonesiana','	irachena','iraniana','irlandese','islandese','israeliana','italiana','ivoriana','	kazka','	keniana','kirgisa','	kosovara','kuatiana','laotiana','lesothiana','lettone','libanese','liberiana','libica','liechtensteiniana','lituana','lussemburghese','macedone','malawiana','maldiviana','malesiana','malgascia','maliana','maltese','marocchina','marshallese','mauritana','mauriziana','messicana','micronesiana','moldava','monegasca','mongola','montenegrina','mozambicana','namibiana','nauruana','neozelandese','nepalese','nigeriana','nigerina','nordcoreana','norvegese','olandese','	omanita','pachistana','palestinese','panamense','papuana','paraguaiana','peruviana','polacca','portoghese','qatariota','romena','ruandese','russa','salvadoregna','samoana','sanmarinese','saudita','senegalese','serba','sierraleonese','singaporiana','siriana','slovacca','slovena','somala','spagnola','statunitense','sudafricana','sudanese','sudcoreana','svedese','svizzera','tagika','tailandese','taiwanese','tanzaniana','tedesca','togolese','tongana','tunisina','turca','turkmena','ucraina','ugandese','ungherese','uruguaiana','uzbeka','vanuatuana','vaticana','venezuelana','vietnamita','	yemenita','zambese','zimbabwese') NOT NULL,
   mail varchar(30) NOT NULL,
   sitoWeb varchar(30) NOT NULL,
-  descrizione varchar(250) NOT NULL,
+  descrizione varchar(500) NOT NULL,
   PRIMARY KEY (nomeCasa)
 )
 
@@ -13,7 +13,7 @@ CREATE TABLE MODELLO_VEICOLO (
   nomeModello varchar(30) NOT NULL,
   annoInizioProduz year(4) NOT NULL,
   annoFineProduz year(4) NOT NULL,
-  descrizione varchar(250) NOT NULL,
+  descrizione varchar(500) NOT NULL,
   casaAutomobilistica VARCHAR(30) NOT NULL,
   PRIMARY KEY (nomeModello),
   FOREIGN KEY (casaAutomobilistica) REFERENCES CASA_AUTOMOBILISTICA(nomeCasa)
@@ -27,7 +27,7 @@ CREATE TABLE VEICOLO (
   prezzo float(7) NOT NULL,
   colore varchar(30) NOT NULL,
   numPorte int(1) NOT NULL,
-  note VARCHAR(250) NOT NULL,
+  note VARCHAR(500) NOT NULL,
   modello VARCHAR(30) NOT NULL,
   PRIMARY KEY (targa),
   FOREIGN KEY (modello) REFERENCES MODELLO_VEICOLO(nomeModello)
